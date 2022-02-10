1f29d0cd95793d7b513d3cbde855db387d4b62ee
AbdelrahmanDwedar <dedou2005@gmail.com>
2022-02-10 02:18:59 +0200

    Changed the theme & items position

--- Celsius to Fehrenheit.py
+++ Celsius to Fehrenheit.py
@@ -1,29 +1,29 @@
 from tkinter import *
 
-# Makes the window 1 (the main window)
+#! Makes the main window
 window = Tk()
 window.title('Celsius To Fehrenheit Convertor')
-window.geometry('700x400+300+140')
+window.geometry('400x200+200+70')
+window.configure(bg="#3f3f3f")
+window.resizable(width=False, height=False) # Unresizable
 
-# make the commands
+#! make the commands
 def Convert():
-    Num = Number.get()
-    Num = float(Num)
-    result = (Num*9/5)+32
-    Label(text=result).place(x=10, y=65)    
+    Num = float(number.get())
+    Label(text=(Num*9/5)+32, bg="#3f3f3f", fg="white").place(x=230, y=60,)
 def kill():
     window.destroy()
 
-#Labels & Entries & Buttons (tools)
-Ask_for_number = Label(text='Enter your Celsius degrees:')
-Number = Entry()
-MyButton = Button(text='convert', command=Convert)
-quit_Button = Button(text='quit', command=kill)
+#! Labels & Entries & Buttons (tools)
+Ask_for_number = Label(text='Enter your Celsius degrees:', bg="#3f3f3f", fg="white", highlightthickness=0)
+number = Entry(bg="#36393f", highlightthickness=0, fg="white")
+MyButton = Button(text='convert', command=Convert, background="#36393f", fg="white", highlightthickness=0)
+quit_Button = Button(text='quit', command=kill, background="#36393f", fg="white", highlightthickness=0)
 
-#sort the page view
+#! sort the page view
 Ask_for_number.place(x=10 ,y=10)
-Number.place(x=70, y=10)
+number.place(x=135, y=10)
 MyButton.place(x=10, y=40)
-quit_Button.place(x=70, y=40)
+quit_Button.place(x=90, y=40)
 
 window.mainloop()
